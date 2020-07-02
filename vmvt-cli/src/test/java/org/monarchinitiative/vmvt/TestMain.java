7ee647188b8a69ef743be0e4606ff265800a4b51
pnrobinson <peter.robinson@jax.org>
2020-07-02 12:40:23 -0400

    refactoring to cli/core

---
+++ vmvt-cli/src/test/java/org/monarchinitiative/vmvt/TestMain.java
@@ -0,0 +1,15 @@
+package org.monarchinitiative.vmvt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TestMain {
+
+    @Test
+    void test (){
+        assertNotNull(Integer.valueOf(42));
+    }
+
+
+}
