2c22ed33fc02f2ed1b2e8d06d37f0739791cad2b
pnrobinson <peter.robinson@jax.org>
2020-07-02 19:49:16 -0400

    adding picocli

--- vmvt-cli/src/main/java/module-info.java
+++ vmvt-cli/src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module org.monarchinitiative.vmvt.cli {
     requires org.monarchinitiative.vmvt.core;
+    requires info.picocli;
 
+    // the following is required for
+    // picocli and junit
     opens org.monarchinitiative.vmvt.cli;
 }
--- vmvt-cli/src/main/java/org/monarchinitiative/vmvt/cli/Main.java
+++ vmvt-cli/src/main/java/org/monarchinitiative/vmvt/cli/Main.java
@@ -1,12 +1,28 @@
 package org.monarchinitiative.vmvt.cli;
 
 /**
- * Main entry point to the command-line interface of vmvt. 
+ * Main entry point to the command-line interface of vmvt.
  */
-public class Main {
 
-public static void main(String [] args){
-    System.out.println("hi");
-}
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.Callable;
+
+@Command(name = "vmvt", mixinStandardHelpOptions = true, version = "vmvt 0.8.0",
+        description = "Variant-motif visualization tool.")
+public class Main implements Callable<Integer> {
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        System.out.printf("HI");
+        return 0;
+    }
 }
